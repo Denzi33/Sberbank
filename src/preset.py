1817e14c4e65d5823d97f2c7b79fb4d26ec128e8
Deniz <68436196+Denzi33@users.noreply.github.com>
2024-08-08 18:23:54 +0300

    Update

--- src/preset.py
+++ src/preset.py
@@ -1,33 +1,94 @@
 # Необходимых модулей для работы:
+import os
+import IPython
 import holidays
 import datetime
 import warnings
+import statsmodels
+import IPython.display
 
 # Необходимые для работы модули под псевдонимами:
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 import seaborn as sns
+import tensorflow as tf
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 # Необходимые сущности (функции, классы, переменные):
+from scipy import stats
 from datetime import date
+from dateutil.rrule import rrule, HOURLY
+from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 from xgboost import XGBRegressor, plot_importance
-from sklearn.model_selection import GridSearchCV, train_test_split 
+from sklearn.linear_model import LinearRegression
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import GridSearchCV, train_test_split
+from statsmodels.tsa.deterministic import DeterministicProcess, CalendarFourier
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 
+from sklearn.impute import KNNImputer, SimpleImputer
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
 
 # Отключение предупреждений:
 pd.options.mode.chained_assignment = None
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
+# Размеры временных периодов:
+hour = 1
+day = hour * 24
+week = day * 7 
+month = day * 30
+year = day * 365
+
+Subj_req = ['Алтайский край', 'Амурская область', 'Архангельская область',
+       'Астраханская область', 'Белгородская область', 'Брянская область',
+       'Владимирская область', 'Волгоградская область', 'Вологодская область',
+       'Воронежская область', 'Еврейская автономная область',
+       'Забайкальский край', 'Западный энергорайон Якутии',
+       'Ивановская область', 'Иркутская область',
+       'Кабардино-Балкарская Республика', 'Калининградская область',
+       'Калужская область', 'Карачаево-Черкесская Республика',
+       'Кемеровская область - Кузбасс', 'Кировская область',
+       'Костромская область', 'Краснодарский край', 'Красноярский край',
+       'Курганская область', 'Курская область', 'Ленинградская область',
+       'Липецкая область', 'Московская область', 'Мурманская область',
+       'Нижегородская область', 'Новгородская область',
+       'Новосибирская область', 'ОЭР Хабаровского края', 'Омская область',
+       'Оренбургская область', 'Орловская область', 'Пензенская область',
+       'Пермский край', 'Приморский край', 'Псковская область',
+       'Республика Башкортостан', 'Республика Бурятия', 'Республика Дагестан',
+       'Республика Ингушетия', 'Республика Калмыкия', 'Республика Карелия',
+       'Республика Коми', 'Республика Крым', 'Республика Марий Эл',
+       'Республика Мордовия', 'Республика Северная Осетия-Алания',
+       'Республика Татарстан (Татарстан)', 'Республика Тыва',
+       'Республика Хакасия', 'Ростовская область', 'Рязанская область',
+       'Самарская область', 'Саратовская область', 'Свердловская область',
+       'Смоленская область', 'Ставропольский край', 'Тамбовская область',
+       'Тверская область', 'Томская область', 'Тульская область',
+       'Тюменская область', 'Удмуртская Республика', 'Ульяновская область',
+       'Центральный энергорайон Якутии', 'Челябинская область',
+       'Чеченская Республика', 'Чувашская Республика - Чувашия',
+       'Южно-Якутский энергорайон', 'Ярославская область'],
 
 # Список дат государственных праздников России:
 rus_holidays = [str(i)[5:] for i in set(holidays.RUS(years=2020).keys())]
 
+# Цвета отображения графиков:
+custom_palette = ["#ff595e", "#ffca3a", "#8ac926", "#1982c4", "#6a4c93"]
+
 # Адрес каталога с файлами данных (относительно базового каталога):
 data_url = "../parsed_data/"
+
+feature_data_url = "../../prepare_data/feature_data.gzip"
 
 # Адрес подготовленных данных (относительно базового каталога):
 prep_data_url = "../prepare_data/data.gzip"
@@ -63,6 +124,39 @@ def get_month(cur_datetime: str) -> int:
     """
 
     return int(cur_datetime.replace(' ', '-').split('-')[1])
+
+
+def create_lag_feature(df: pd.DataFrame, step_count: int) -> pd.Series:
+    """
+    
+    """
+    
+    temp_df = pd.DataFrame()
+
+    for subj in df["subject_name"].unique():
+        subj_df = df[df["subject_name"] == subj].copy()
+        subj_df["actual_consumption"] = subj_df["actual_consumption"].shift(step_count)
+        temp_df = temp_df.append(subj_df)
+        
+    temp_df.sort_values(by=["datetime", "subject_name"], inplace=True)
+    
+    return temp_df["actual_consumption"]
+
+def create_time_dummie_feature(df: pd.DataFrame) -> pd.Series:
+    """
+    
+    """
+    
+    temp_df = pd.DataFrame()
+
+    for subj in df["subject_name"].unique():
+        subj_df = df[df["subject_name"] == subj].copy()
+        subj_df["time_dummie"] = range(1, subj_df.shape[0] + 1)
+        temp_df = temp_df.append(subj_df)
+        
+    temp_df.sort_values(by=["datetime", "subject_name", "time_dummie"], inplace=True)
+    
+    return temp_df["time_dummie"]
 
 
 def get_day_month(cur_datetime: str) -> int:
@@ -195,16 +289,24 @@ def to_int(sample: str) -> float:
 
     return np.nan if '-' in sample else int(sample.replace(' ', '').rstrip("МВт*ч"))
 
+
 # Функция оценки прогноза модели:
 def check_res(y_pred: np.array, y_true: np.array) -> None:
     """
     Prints all necessary metrics for time series.
     
-    
-    
+    :param y_pred: np.array
+        A predicted values of the target variable.
+
+    :param y_true: np.array
+        A real values of the target variable.
+
+    :return: None.
     """
-    
-    print("Mean squared error on test set: ", mean_squared_error(y_true, y_pred))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_true, y_pred))
-    print("Mean percentage absolute error on test set: ", mean_absolute_percentage_error(y_true, y_pred))
+
+    print(f"""Значения метрик:
+                              MSE - {mean_squared_error(y_true, y_pred)}
+                              MAE - {mean_absolute_error(y_true, y_pred)}
+                              MAPE - {mean_absolute_percentage_error(y_true, y_pred)}"""
+         )
     
---
+++ src/utils_functions.py
@@ -0,0 +1,165 @@
+"""
+------------------------------------------------------------------------------------------------------------------------
+The module contains utils functions.
+------------------------------------------------------------------------------------------------------------------------
+ASYNC Functions:
+------------------------------------------------------------------------------------------------------------------------
+    clean_data:
+        Description: Deletes unnecessary row columns.
+
+        Parameters:
+            data_rows - A list of dictionaries to clean,
+            del_columns - A list of columns to delete,
+            inplace - A flag of filling in-place.
+
+        Parameters type:
+            list | None,
+            list | None,
+            bool [False].
+
+        Returns: A list of cleared data rows.
+
+        Return type: list | None.
+
+    make_request:
+        Description: Makes requests to the specified address.
+
+        Parameters:
+            url - A request link,
+            verify - A site security flag.
+
+        Parameters type:
+            str,
+            bool [False].
+
+        Returns: Response as a data dictionary.
+
+        Return type: dict | None.
+------------------------------------------------------------------------------------------------------------------------
+Functions:
+------------------------------------------------------------------------------------------------------------------------
+    csv_to_gzip:
+        Description: Сonvert .csv files to .gzip files.
+
+        Parameters:
+            start_year - The first year for conversion,
+            end_year - The last year for conversion,
+            rel_path - A relative file path.
+
+        Parameters type:
+            int,
+            int,
+            str.
+
+        Return type: None.
+    
+    read_json:
+        Description: Returns json file data as dictionary.
+
+        Parameters: json_file_name - A json file name.
+
+        Parameters type: str.
+
+        Returns: A data dictionary.
+
+        Return type: dict | None.
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+# Necessary modules:
+import json
+
+# Necessary modules with alias:
+import pandas as pd
+
+# Necessary functions:
+from itertools import product
+
+# Necessary functions and variables with alias:
+from aiohttp import ClientSession as ClSess, TCPConnector as Conn
+
+
+async def clean_data(
+                     data_rows: list | None,
+                     del_columns: list | None,
+                     inplace: bool = False
+) -> list | None:
+    """
+    Deletes unnecessary row columns.
+
+    :param data_rows: list | None
+        A list of dictionaries to clean.
+    :param del_columns: list | None
+        A list of columns to delete.
+    :param inplace: bool [False]
+        A flag of filling in-place.
+
+    :return: list | None
+        A list of cleared data rows.
+    """
+
+    # Inplace functionality case:
+    prep_data: list | None = data_rows if inplace is True else data_rows.copy()
+
+    # Delete useless columns:
+    [row.pop(column, default=0) for row, column in product(prep_data, del_columns)]
+
+    return prep_data
+
+
+async def make_request(url: str, verify: bool = False) -> dict | None:
+    """
+    Makes requests to the specified address.
+
+    :param url: str
+        A request link,
+    :param verify: bool [False]
+        A site security flag.
+
+    :return: dict | None
+        Response as a data dictionary.
+    """
+
+    # Create session for async requests:
+    async with ClSess(connector=Conn(ssl=verify)) as session:
+        async with session.get(url) as resp:
+            return await resp.json() if resp.status == 200 else None
+
+def csv_to_gzip(
+                start_year: int,
+                end_year: int,
+                rel_path: str
+) -> None:
+    """
+    Convert .csv files to .gzip files.
+
+    :param start_year: int
+        The first year for conversion.
+    :param end_year: int
+        The last year for conversion.
+    :param rel_path: str
+        A relative file path.
+
+    :return: None
+    """
+
+    [pd.read_csv(rel_path + f"{year}.csv").to_parquet(rel_path + f"{year}.gzip") for
+     year in range(start_year, end_year)]
+
+
+def read_json(json_file_name: str) -> dict | None:
+    """
+    Returns .json file data as dictionary.
+
+    :param json_file_name: str
+        A .json file name.
+
+    :return: dict | None
+        A data dictionary.
+    """
+
+    try:
+        # Take json data:
+        return json.load(open(json_file_name))
+    except Exception as err:
+        print(err)
